2e3653e5782821dddf41c93c3339d82590120b60
himanshu.sharma9 <himanshu.sharma@ekank.com>
2022-11-13 21:47:27 +0530

    fix issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: [ 'plugin:prettier/recommended', , 'next/core-web-vitals'],
+  extends: ['plugin:prettier/recommended', 'next/core-web-vitals'],
   rules: {
     'prettier/prettier': 'error',
     'react/react-in-jsx-scope': 'off',
