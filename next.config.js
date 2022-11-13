cb538540229e95ea0c71e68e9c011fdd804cfd9b
himanshu.sharma9 <himanshu.sharma@ekank.com>
2022-11-13 21:41:19 +0530

    fix issues

--- next.config.js
+++ next.config.js
@@ -81,7 +81,6 @@ module.exports = withBundleAnalyzer({
         'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
         react: 'preact/compat',
         'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
       })
     }
 
